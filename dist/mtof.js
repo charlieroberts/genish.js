13a56ca498da6490688b01799d407aa422c79b87
charlieroberts <charlie@charlie-roberts.com>
2016-05-09 21:12:37 -0400

    try again: mtof w/ test

---
+++ dist/mtof.js
@@ -0,0 +1,36 @@
+'use strict';
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var _gen = require('./gen.js');
+
+var proto = {
+  name: 'mtof',
+
+  gen: function gen() {
+    var out = void 0,
+        inputs = _gen.getInputs(this);
+
+    if (isNaN(inputs[0])) {
+      _gen.closures.add(_defineProperty({}, this.name, Math.exp));
+
+      out = '( ' + this.tuning + ' * gen.exp( .057762265 * (' + inputs[0] + ' - 69) ) )';
+    } else {
+      out = this.tuning * Math.exp(.057762265 * (inputs[0] - 69));
+    }
+
+    return out;
+  }
+};
+
+module.exports = function (x, props) {
+  var ugen = Object.create(proto),
+      defaults = { tuning: 440 };
+
+  if (props !== undefined) Object.assign(props.defaults);
+
+  Object.assign(ugen, defaults);
+  ugen.inputs = [x];
+
+  return ugen;
+};
