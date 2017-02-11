8852dfb1d1c749acf1c74d07c9bfbf02765a9379
charlieroberts <charlie@charlie-roberts.com>
2017-02-11 17:52:11 -0500

    ltp and gtp > asm.js

--- dist/ltp.js
+++ dist/ltp.js
@@ -3,14 +3,20 @@
 let gen  = require('./gen.js')
 
 let proto = {
-  name:'ltp',
+  basename:'ltp',
 
   gen() {
     let out,
         inputs = gen.getInputs( this )
 
     if( isNaN( this.inputs[0] ) || isNaN( this.inputs[1] ) ) {
-      out = `(${inputs[ 0 ]} * (( ${inputs[0]} < ${inputs[1]} ) | 0 ) )` 
+      gen.variableNames.add( [this.name, 'f'] )
+
+      out = [
+        this.name,
+        `  ${this.name} = fround( ${inputs[ 0 ]} * fround( (${inputs[0]} < ${inputs[1]} ) |0 ) )\n` 
+      ]
+
     } else {
       out = inputs[0] * (( inputs[0] < inputs[1] ) | 0 )
     }
@@ -23,6 +29,8 @@ module.exports = (x,y) => {
   let ltp = Object.create( proto )
 
   ltp.inputs = [ x,y ]
+
+  ltp.name = ltp.basename + gen.getUID()
 
   return ltp
 }
--- js/gtp.js
+++ js/gtp.js
@@ -3,16 +3,22 @@
 let gen  = require('./gen.js')
 
 let proto = {
-  name:'gtp',
+  basename:'gtp',
 
   gen() {
     let out,
         inputs = gen.getInputs( this )
 
     if( isNaN( this.inputs[0] ) || isNaN( this.inputs[1] ) ) {
-      out = `(${inputs[ 0 ]} * ( ( ${inputs[0]} > ${inputs[1]} ) | 0 ) )` 
+      gen.variableNames.add( [this.name, 'f'] )
+
+      out = [
+        this.name,
+        `  ${this.name} = fround( ${inputs[ 0 ]} * fround( (${inputs[0]} > ${inputs[1]} ) |0 ) )\n` 
+      ]
+
     } else {
-      out = inputs[0] * ( ( inputs[0] > inputs[1] ) | 0 )
+      out = inputs[0] * (( inputs[0] > inputs[1] ) | 0 )
     }
     
     return out
@@ -23,6 +29,8 @@ module.exports = (x,y) => {
   let gtp = Object.create( proto )
 
   gtp.inputs = [ x,y ]
+
+  gtp.name = gtp.basename + gen.getUID()
 
   return gtp
 }
