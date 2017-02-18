971bf0bed01e601bf91dc26ccbb0a1a9e4d72693
charlieroberts <charlie@charlie-roberts.com>
2017-02-18 17:26:37 -0500

    switch > asm.js

--- js/switch.js
+++ js/switch.js
@@ -9,10 +9,9 @@ let proto = {
     let inputs = gen.getInputs( this ), out
 
     if( inputs[1] === inputs[2] ) return inputs[1] // if both potential outputs are the same just return one of them
-    
-    out = `  var ${this.name}_out = ${inputs[0]} === 1 ? ${inputs[1]} : ${inputs[2]}\n`
-
-    gen.memo[ this.name ] = `${this.name}_out`
+     
+    gen.variableNames.add( [ this.name + '_out', 'f' ] )
+    out = `  ${this.name}_out = ${inputs[0]} == fround(1|0) ? fround(${inputs[1]}) : fround(${inputs[2]})\n`
 
     return [ `${this.name}_out`, out ]
   },
