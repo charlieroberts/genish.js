968cd4cde5b7523df26ef495ed3a4184282214cb
Abram Hindle <abram.hindle@ualberta.ca>
2017-06-06 14:14:01 -0600

    tanh in dist

---
+++ dist/tanh.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var _gen = require('./gen.js');
+
+var proto = {
+  basename: 'tanh',
+
+  gen: function gen() {
+    var out = void 0,
+        inputs = _gen.getInputs(this);
+
+    if (isNaN(inputs[0])) {
+      _gen.closures.add({ 'tanh': Math.tanh });
+
+      out = 'gen.tanh( ' + inputs[0] + ' )';
+    } else {
+      out = Math.tanh(parseFloat(inputs[0]));
+    }
+
+    return out;
+  }
+};
+
+module.exports = function (x) {
+  var tanh = Object.create(proto);
+
+  tanh.inputs = [x];
+  tanh.id = _gen.getUID();
+  tanh.name = tanh.basename + '{tanh.id}';
+
+  return tanh;
+};
