a3eabc67a22536c77d0952124a4c68bab5931f47
charlieroberts <charlie@charlie-roberts.com>
2017-08-18 13:49:45 -0400

    added exp ugen and modified .gitignore

---
+++ js/exp.js
@@ -0,0 +1,31 @@
+'use strict'
+
+let gen  = require('./gen.js')
+
+let proto = {
+  name:'exp',
+
+  gen() {
+    let out,
+        inputs = gen.getInputs( this )
+
+    if( isNaN( inputs[0] ) ) {
+      gen.closures.add({ [ this.name ]: Math.exp })
+
+      out = `gen.exp( ${inputs[0]} )`
+
+    } else {
+      out = Math.exp( parseFloat( inputs[0] ) )
+    }
+    
+    return out
+  }
+}
+
+module.exports = x => {
+  let exp = Object.create( proto )
+
+  exp.inputs = [ x ]
+
+  return exp
+}
