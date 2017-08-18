deff65d43712be481394aa4dc6b8181413dabd6d
charlieroberts <charlie@charlie-roberts.com>
2017-08-18 13:39:16 -0400

    some refactoring of delay

--- js/delay.js
+++ js/delay.js
@@ -6,7 +6,8 @@ const gen  = require( './gen.js'  ),
       peek = require( './peek.js' ),
       sub  = require( './sub.js'  ),
       wrap = require( './wrap.js' ),
-      accum= require( './accum.js')
+      accum= require( './accum.js'),
+      memo = require( './memo.js' )
 
 const proto = {
   basename:'delay',
@@ -20,29 +21,30 @@ const proto = {
   },
 }
 
-const defaults = { size: 512, feedback:0, interp:'none' }
+const defaults = { size: 512, interp:'none' }
 
 module.exports = ( in1, taps, properties ) => {
-  let ugen = Object.create( proto ),
-      writeIdx, readIdx, delaydata
+  const ugen = Object.create( proto )
+  let writeIdx, readIdx, delaydata
 
   if( Array.isArray( taps ) === false ) taps = [ taps ]
   
-  let props = Object.assign( {}, defaults, properties )
+  const props = Object.assign( {}, defaults, properties )
 
-  if( props.size < Math.max( ...taps ) ) props.size = Math.max( ...taps )
+  const maxTapSize = Math.max( ...taps )
+  if( props.size < maxTapSize ) props.size = maxTapSize
 
   delaydata = data( props.size )
   
   ugen.inputs = []
 
-  writeIdx = accum( 1, 0, { max:props.size }) 
+  writeIdx = accum( 1, 0, { max:props.size, min:0 })
   
   for( let i = 0; i < taps.length; i++ ) {
     ugen.inputs[ i ] = peek( delaydata, wrap( sub( writeIdx, taps[i] ), 0, props.size ),{ mode:'samples', interp:props.interp })
   }
   
-  ugen.outputs = ugen.inputs // ugn, Ugh, UGH! but i guess it works.
+  ugen.outputs = ugen.inputs // XXX ugh, Ugh, UGH! but i guess it works.
 
   poke( delaydata, in1, writeIdx )
 
