174aeb1c2374e462563e89d9875a2da3915c9c3b
charlieroberts <charlie@charlie-roberts.com>
2021-06-08 13:35:08 -0700

    updating gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,12 @@
-var gulp = require('gulp'),
-    //uglify   = require('gulp-uglify'),
-    notify     = require('gulp-notify'),
-    babel      = require('gulp-babel'),
-    browserify = require('browserify'),
-    buffer     = require('gulp-buffer'),
-    source     = require('vinyl-source-stream'),
-    babelify   = require('babelify'),
-    mocha      = require('gulp-mocha')
+const gulp = require( 'gulp' )
+const wat2wasm = require( 'gulp-wat2wasm' )
 
-gulp.task( 'js', function() {
-  browserify({ debug:true, standalone:'genish' })
-    .require( './js/index.js', { entry: true } )
-    .transform( babelify, { presets:['es2015'] })
-    .bundle()
-    .pipe( source('gen.lib.js') )
+const build = function( done ) {
+  gulp.src('./src/*.wat')
+    .pipe( wat2wasm() )
     .pipe( gulp.dest('./dist') )
-    //.pipe( uglify() )
-    //.pipe( gulp.dest('./dist') )
-    .pipe(
-      notify({
-        message:'Build has been completed',
-        onLast:true
-      })
-    )
+  
+  done()
+}
 
-  // transpile (but don't browserify) for use with node.js tests
-  return gulp.src( './js/**.js' )
-    .pipe( babel({ presets:['es2015'] }) )
-    .pipe( gulp.dest('./dist' ) )
-
-})
-
-gulp.task( 'test', ['js'], ()=> {
-  return gulp.src('test/*.js', {read:false})
-    .pipe( mocha({ reporter:'nyan' }) ) // spec, min, nyan, list
-})
-
-
-gulp.task( 'watch', function() {
-  gulp.watch( './js/**.js', ['test'] )
-})
-
-gulp.task( 'default', ['js','test'] )
+exports.default = build
