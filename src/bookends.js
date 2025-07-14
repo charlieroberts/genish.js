83e9a782669146960145e4a09787555824cc559b
charlieroberts <charlie@charlie-roberts.com>
2025-07-14 06:56:02 -0700

    add func signatures, logf logi return input values

--- src/bookends.js
+++ src/bookends.js
@@ -2,13 +2,23 @@ const front = function( memAmount = 50) {
 
 const code = `(module
   (import "env" "memory" (memory $mem ${memAmount} ${memAmount} shared))
-  (import "env" "_logi" (func $_logi (param i32) ) ) 
-  (import "env" "_logf" (func $_logf (param f32) ) ) 
+  ;;(import "env" "memory" (memory $mem ${memAmount} ${memAmount} ))
+  (import "env" "_logi" (func $_logi (param i32)  (result i32) ) ) 
+  (import "env" "_logf" (func $_logf (param f32)  (result f32) ) ) 
 
 
   (global $sr (import "env" "sr") f32)
   (export "memory" (memory $mem) )
   (global $clock (mut i32) (i32.const 0))
+
+  ;; for noise function
+  (type $t2 (func (param f64 i32) (result f64)))
+  ;; primary function signature used
+  (type $sig-i32--f32 (func (param $loc i32) (result f32) ) )
+  ;; logic functions
+  (type $sig-i32--i32 (func (param $loc i32) (result i32) ) )
+  ;; no return
+  (type $sig-i32 (func (param $loc i32) ) )
 `
 
 return code
