09ac9b1d85739f7d2b1e46a0a3bd0ff9358ceae6
charlieroberts <charlie@charlie-roberts.com>
2025-07-14 15:29:07 -0700

    add comments and reformat

--- src/ugens/accum.js
+++ src/ugens/accum.js
@@ -4,12 +4,13 @@ const accum = function( obj, offset=0 ) {
   let memlength     = obj.__memoryLength * 4,
       incr_prop     = null,
       incr_compiled = null,
-      resetblock    = null
+      resetblock    = null,
+      reset_compiled= null
 
   const phase_offset = 20,
-        phase_id     = '$phase'+obj.idx,
-        memory_loc   = '$memory'+obj.idx,
-        phase_loc    = '$phaseloc'+obj.idx,
+        phase_id     = '$accumphase'+obj.idx,
+        memory_loc   = '$accummemoryloc'+obj.idx,
+        phase_loc    = '$accumphaseloc'+obj.idx,
         out_id       = '$accumout'+obj.idx
 
   if( obj.__flags[0] ) {
@@ -22,7 +23,7 @@ const accum = function( obj, offset=0 ) {
   }
 
   if( obj.__flags[1] ) {
-    const reset_compiled = gen.compile( obj.reset, memlength + offset )
+    reset_compiled = gen.compile( obj.reset, memlength + offset )
     memlength            += reset_compiled.memlength
     offset               += reset_compiled.memlength
   }
@@ -32,29 +33,48 @@ const accum = function( obj, offset=0 ) {
   gen.addLocal(`(local ${phase_id} f32)`) 
   gen.addLocal(`(local ${out_id} f32)`)
 
+  const getReset = function() {
+    const resetBlock = 
+  `  ;; accum: reset
+  ${ reset_compiled.string }\n
+  if (result f32)
+    ;; set phase.value to $min
+    (f32.store
+      (local.get ${phase_loc}) 
+      (f32.const ${obj.min})   
+    ) 
+    (f32.const ${obj.min})
+    local.set ${out_id}
+  end
+  `
+    return resetBlock 
+  }
   const incrblock = 
 `
+${obj.__flags[1] === 0 ? `;;;;;;;; begin accum ;;;;;;;;` : '' }
 i32.const ${offset}
 local.get $loc
 i32.add
 local.tee ${memory_loc}
 
-;; load phase
+;; accum: load phase
 i32.const ${ phase_offset }
 i32.add
 local.tee ${phase_loc}
 f32.load
 local.tee ${out_id}
 
-;; phase increment
+${obj.__flags[1] ? getReset() : '' }
+
+;; accum: phase increment
 ${incr_prop}
 f32.add
 local.set ${phase_id}
 
-;; push phase idx for set-property to the stack
+;; accum: push phase idx for set-property to the stack
 local.get ${ phase_loc }
 
-;; wrap phase 
+;; accum: wrap phase 
 (f32.lt (local.get ${phase_id}) (f32.const ${ obj.max }))
 if (result f32)
   (local.get ${phase_id})
@@ -68,25 +88,11 @@ end
 
 f32.store
 local.get ${out_id}
+
+;;;;;;;; end accum ;;;;;;;;
 `
 
-  if( obj.__flags[1] ) {
-    resetBlock = 
-`
-${ reset_compiled.string }\n
-if (result f32)
-  ;; set phase.value to $min
-  (f32.store
-    (local.get ${phase_loc}) 
-    (f32.const ${obj.min})   
-  ) 
-  (f32.const ${obj.min})
-  local.set ${out_id}
-end
 
-${incrblock}
-`
-  }
 
   memlength += 4
   const out = {
