4b37d9d8ca63c5c5ef33baae2298c7a63abb8af4
charlieroberts <charlie@charlie-roberts.com>
2025-07-28 16:58:24 -0700

    monops

---
+++ src/ugens/monops.js
@@ -0,0 +1,52 @@
+let gen
+
+// use arrays for times when gen name is different from wat name
+// TODO: why is eq / neq in both arithmetic and logic?
+const callops = [ 'sin', 'cos', 'tan', 'asin', 'acos', 'atan', 'tanh', ['noise', 'random']  ],
+      nativeops = [ 'floor', 'ceil', 'abs', ['round', 'nearest'] ],
+      monops = {}
+
+const allops = callops.concat( nativeops ).flat()
+
+for( let __op of allops ) {
+  const changeName = Array.isArray( __op )
+  const op = changeName ? __op[ 1 ] : __op
+  monops[ op ] = function( obj, offset=0 ) {
+    let memlength = 4,
+        input_compiled
+
+    if( obj.__flags[0] ) {
+      input_compiled = gen.compile( obj[0] )//, memlength + offset )
+      memlength += input_compiled.memlength
+    }else{
+      input_prop = `f32.const ${obj[0]}`
+    }
+
+    let string = `${input_compiled.string}\n`
+    if( callops.flat().indexOf( op ) !== -1 ) {
+      string += `call $_${op}\n`
+    }else{
+      string += `f32.${op}\n`
+    }
+
+    if( obj.__shouldMemo ) {
+      const name = obj.__memoName 
+      gen.addLocal(`(local $${name} f32)` )
+      string += `local.tee $${name}\n`
+    }
+
+    const out = {
+      memlength,
+      string
+    }
+  
+    return out
+  }
+}
+
+const monops_module = __gen => {
+  gen = __gen
+  return monops
+}
+
+export default monops_module
