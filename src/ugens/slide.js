3ed0e2cc58dda04d6edd668ed6ef8aba84adb08d
charlieroberts <charlie@charlie-roberts.com>
2025-07-28 17:02:08 -0700

    slide ugen

---
+++ src/ugens/slide.js
@@ -0,0 +1,28 @@
+let gen
+
+import { phasor,peek,accum, div, ssd, gt, ifelse } from '../main.js'
+import utilities from '../utilities.js'
+
+const compile = function( obj, offset=0 ) {
+  let y1 = ssd(0),
+      filter = null, 
+      slideAmount = null
+
+  //y (n) = y (n-1) + ((x (n) - y (n-1))/slide) 
+  slideAmount = ifelse( gt( obj.input, y1.out), obj.slideUp, obj.slideDown )
+
+  // filter = memo( add( y1.out, div( sub( in1, y1.out ), slideAmount ) ) )
+  filter = add( y1.out, div( sub( obj.input, y1.out ), slideAmount ) ).memo() 
+
+  y1.in( filter )
+  filter.__memoName = obj.__memoName
+
+  return gen.compile( filter, offset )
+}
+
+const module = __gen => {
+  gen = __gen
+  return compile
+}
+
+export default module
