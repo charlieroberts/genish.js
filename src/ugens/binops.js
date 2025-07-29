2753043d6e3ac23af73d30f2127d3f23e27593eb
charlieroberts <charlie@charlie-roberts.com>
2025-07-28 17:00:34 -0700

    memo binops

--- src/ugens/binops.js
+++ src/ugens/binops.js
@@ -57,10 +57,17 @@ for( let optype in opcategories ) {
       }else{
         y_prop = `f32.const ${obj[1]}`
       }
-    
+      
+      let string = blocks[ optype ]( x_prop, y_prop, changeName ? __op[1] : op )
+      if( obj.__shouldMemo ) {
+        const name = obj.__memoName 
+        gen.addLocal(`(local $${name} f32)` )
+        string += `local.tee $${name}\n`
+      }
+
       const out = {
         memlength,
-        string: blocks[ optype ]( x_prop, y_prop, changeName ? __op[1] : op )
+        string
       }
     
       return out
