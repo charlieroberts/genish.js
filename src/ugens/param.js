e700a60760294d77a6b401efa1466f81da95937e
charlieroberts <charlie@charlie-roberts.com>
2025-08-07 15:23:49 -0700

    removing log

--- src/ugens/param.js
+++ src/ugens/param.js
@@ -23,7 +23,6 @@ const param_module = __gen => {
     }
 
     if( obj.__shouldMemo ) {
-      console.log( 'PARAM MEMO:', obj.__memoName )
       gen.addLocal(`(local ${obj.__memoName} f32)`)
       ugen.string += `\nlocal.tee ${obj.__memoName}\n`
     }
