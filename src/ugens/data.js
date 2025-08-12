6cb99e335acce13fee41f1533f5c93eec1d89b26
charlieroberts <charlie@charlie-roberts.com>
2025-08-12 11:45:48 -0700

    formatting

--- src/ugens/data.js
+++ src/ugens/data.js
@@ -1,37 +1,41 @@
 import utilities from '../utilities.js'
-let gen
 
 // TODO: use actual samplerate
 const compile = function( obj, offset = 0 ) {
-  const out = { 
-    value: obj.value, 
-    length: typeof obj.value === 'object' ? obj.value.length : 1,
-    name: obj.name
-  }
+  let out = null
+  if( obj.out === undefined ) {
+    out = { 
+      value: obj.value, 
+      length: typeof obj.value === 'object' ? obj.value.length : 1,
+      name: obj.name
+    }
 
-  // if value is number, it is the number of memory slots
-  // to reserve, NOT THE ACTUAL DATA VALUE
-  if( typeof obj.value === 'number' ) {
-    out.idx = utilities.getMemory( obj.value, 'data' )
+    // if value is number, it is the number of memory slots
+    // to reserve, NOT THE ACTUAL DATA VALUE
+    if( typeof obj.value === 'number' ) {
+      out.idx = utilities.getMemory( obj.value, 'data' )
+    }else{
+      // if an array is passed, copy it to memory
+      out.idx = utilities.getMemory( out.value.length, 'data' )
+      utilities.memf.set( out.value, out.idx )
+    }
+
+    // needed so that data is not recompiled when using
+    // across multiple peeks / pokes... this places the .idx
+    // property on the user-facing instance
+    obj.idx = out.idx
+
+    out.__static = obj.__static !== undefined ? obj.__static : true
+    obj.__memoName = '$data'+obj.idx
+    obj.out = out
   }else{
-    // if an array is passed, copy it to memory
-    out.idx = utilities.getMemory( out.value.length, 'data' )
-    utilities.memf.set( out.value, out.idx )
+    out = obj.out
   }
-
-  // needed so that data is not recompiled when using
-  // across multiple peeks / pokes... this places the .idx
-  // property on the user-facing instance
-  obj.idx = out.idx
-
-  out.__static = obj.__static !== undefined ? obj.__static : true
-  obj.__memoName = '$data'+obj.idx
   
   return out
 }
 
-const module = __gen => {
-  gen = __gen
+const module = _ => {
   return compile
 }
 
