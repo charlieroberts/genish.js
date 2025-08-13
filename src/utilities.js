8f0d79117e4b5028d3c3e099136995998754b45c
charlieroberts <charlie@charlie-roberts.com>
2025-08-13 11:01:33 -0700

    removing play / factory method from utiilities

--- src/utilities.js
+++ src/utilities.js
@@ -1,5 +1,4 @@
 import { data } from './main.js'
-//import gen from './gen.js'
 
 let memf, memi,
     m = 0,
@@ -9,6 +8,7 @@ const utilities = {
   buffers: {},
   sampleRate: null,
   __debugMemory: false,
+  ctx: null,
 
   getMemoryIndex() { return m },
 
@@ -79,7 +79,7 @@ const utilities = {
     const promise = new Promise( (resolve,reject) => {
       if( !isLoaded ) {
         req.onload = function() {
-          var audioData = req.response
+          const audioData = req.response
 
           utilities.ctx.decodeAudioData( audioData, buffer => {
             utilities.buffers[ soundFilePath ] = buffer.getChannelData(0)
@@ -99,7 +99,7 @@ const utilities = {
     return promise
   },
   
-  createWavetables() {  
+  createWavetables( location=1 ) {  
     /* sine */
     let sinebuffer = new Float32Array( 1024 )
 
@@ -107,8 +107,9 @@ const utilities = {
       sinebuffer[ i ] = Math.sin( ( i / l ) * ( Math.PI * 2 ) )
     }
 
-    utilities.sinedata = data( sinebuffer )
+    utilities.sinedata = data( sinebuffer ) 
     utilities.sinedata.__static = true
+
 
     /* pan */ 
     // let bufferL = new Float32Array( 1024 ),
@@ -123,135 +124,8 @@ const utilities = {
 
     // utilities.panL = data( bufferL )
     // utilities.panR = data( bufferR )
-  },
+  }
 
-  play( ugen ) {
-    window.out = ugen
-  
-    if( Array.isArray( ugen ) ) {
-      node.port.postMessage({
-        address:'renderStereo',
-        left: {
-          loc:ugen[0].idx*4,
-        },
-        right: {
-          loc:ugen[1].idx*4,
-        }
-      })
-    }else{
-      node.port.postMessage({
-        address:'render',
-        loc:ugen.idx*4
-      })
-    }
-  },
-
-  factory( props, statics, baseidx, name, isComposite=false ) {
-    const obj = { name },
-          keys = Object.keys( props ),
-          statickeys = Object.keys( statics )
-
-    // dynamic version needs to store function id for 
-    // each ugen, with compiled ugens this is unnecessary
-    const IS_DYNAMIC = 0
-  
-    // function id, properties, statics
-    obj.idx = utilities.getMemory( IS_DYNAMIC + keys.length + statickeys.length )
-  
-    // initial binary signature
-    const initSig = Object.values( props ).reduce(
-      (accum,val) => accum + ( isNaN(val) ? 1 : 0 ), 
-      '0b'
-    )
-    
-    // array of bits to twiddle
-    const flags = Object.values( props ).map( v => isNaN  ( v ) ? 1 : 0 )
-  
-    let __fid = Object.values( props ).length >= 1 
-      ? baseidx + Number( initSig )
-      : baseidx
-  
-    if( IS_DYNAMIC ) {
-      Object.defineProperty( obj, 'fid', { 
-        get() { return __fid },
-        set(v) {
-          __fid = v
-          memi[ obj.idx ] = __fid
-        }
-      })
-    }
-    
-    obj.fid = __fid
-
-    const offset = obj.offset || 0
-    for( let i = 0; i < keys.length; i++ ) {
-      const key = keys[ i ]
-      const idx = obj.idx + IS_DYNAMIC + i + offset
-  
-      let value = props[ key ]
-      Object.defineProperty( obj, key, {
-        get() { return value },
-        // get() { return memf[ idx * 4 ] },
-        set( v ) {
-          // is this a number or a ugen?
-          const isUgen = isNaN( v ) ? 1 : 0
-          // set flag for determining function signature
-          flags[ i ] = isUgen
-          // get number for signature
-          const sig = Number( flags.reduce( (accum,val)=>accum+val, '0b') )
-  
-          obj.fid = baseidx + sig
-          
-          if( isUgen ) {
-            memi[ idx ] = v.idx * 4
-          }else{
-            memf[ idx ] = v
-          }
-          value = v
-        }
-      })
-  
-      obj[ key ] = props[ key ]
-    }
-  
-    let staticidx = obj.idx + IS_DYNAMIC + keys.length
-    for( let key of statickeys ) {
-      const idx = staticidx+offset
-      Object.defineProperty( obj, key, {
-        get() {
-          const out = statics[ key ].type === 'f'
-            ? memf[ idx ]
-            : memi[ idx ]
-          
-          return out
-        },
-        set(v) {
-          if( statics[ key ].type === 'f' )
-            memf[ idx ] = v
-          else
-            memi[ idx ] = v
-        }
-      })
-      obj[ key ] = statics[ key ].value
-      staticidx++
-    }
-  
-    obj.__flags = flags
-    obj.__props = props
-    obj.__statics = statics
-    obj.__memoryLength = keys.length + Object.keys( statickeys ).length + IS_DYNAMIC
-    obj.__shouldMemo = false
-    obj.__memoName =  obj.name + '_' + obj.idx + '_memo'
-
-    obj.memo = ()=> {
-      obj.__shouldMemo = true
-      return obj
-    }
-  
-    return obj
-  },
-
-  
 }
 
 export default utilities
