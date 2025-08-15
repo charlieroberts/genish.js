4b87039286f4dfe7de3cad54d35b559da5655716
charlieroberts <charlie@charlie-roberts.com>
2025-08-15 11:30:23 -0700

    fix memoing for history output

--- src/ugens/history.js
+++ src/ugens/history.js
@@ -6,13 +6,14 @@ import poke from './poke.js'
 let __poke = null
 
 const compile = function( obj, offset = 0 ) {
-  obj.__data = data([obj.value])
-  
+  obj.__data = data([ obj.value ])
+
   // triggers compilation of poke in current ugen
   __poke( obj.__data, obj.__input, offset  )
 
   const out = peek( obj.__data, 0, 0, 0 ) 
   out.__memoName =  obj.__memoName
+  out.__shouldMemo = obj.__shouldMemo
   
   return gen.compile( out, offset )
 }
